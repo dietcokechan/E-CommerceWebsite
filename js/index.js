0364fb436ab26a321c7f681a014e15fa230b46d5
dietcokechan <dietcokechan@gmail.com>
2022-02-06 16:05:26 +0200

    combined all js code in index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+// scroll to top function
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop()
+{
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// slideshow section
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -5,12 +15,6 @@ showSlides(slideIndex);
 function plusSlides(n)
 {
   showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n)
-{
-  showSlides(slideIndex = n);
 }
 
 function showSlides(n)
