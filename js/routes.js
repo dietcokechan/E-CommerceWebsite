c930609fad9cc8ef2a78f45edd51e8f085db3a88
dietcokechan <dietcokechan@gmail.com>
2022-03-03 11:41:12 +0200

    endpoints

---
+++ js/routes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const model = require("./models");
+const app = express();
+
+// endpoints
+app.post("/Product", (req, res) => {
+
+})
+
+app.get("/Products", (req, res) => {
+
+})
+
+// endpoint for customers
+app.post("/Client", (req, res) => {
+
+})
+
+app.get("/Clients", (req, res) => {
+
+})
+
+// endpoint for orders
+app.post("/Order", (req, res) => {
+
+})
+
+app.get("/Orders", (req, res) => {
+
+})
