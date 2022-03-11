171d79200206fd5077f80dff0b6913ac342032ab
dietcokechan <dietcokechan@gmail.com>
2022-03-11 15:29:05 +0200

    added body parser

--- js/server.js
+++ js/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Router = require('./routes');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+// connect to db
 mongoose.connect('mongodb+srv://88metalsDB:Gd7cE37qVG2kF6i7@88metals.jmaw4.mongodb.net/88metals', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
